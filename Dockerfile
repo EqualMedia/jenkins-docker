d28832eec1312c010119f2b161d353a8b0f24af1
Vaidas Jablonskis <jablonskis@gmail.com>
2014-10-30 14:36:13 +0000

    improve backup/restore
    - tarball instead of raw files, because s3 does not support symlinks
    - add jenkins_backup.sh script to /srv/jenkins/jenkins_backup.sh
    - write out /etc/jenkins-bucket-config file

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ FROM fedora:20
 RUN yum update -y -q; yum clean all
 RUN yum install -y -q python-pip java-headless dejavu-sans-fonts git wget docker-io; yum clean all; pip install awscli
 
-ENV JENKINS_VERSION 1.586
+ENV JENKINS_VERSION 1.587
 RUN yum install -y -q http://pkg.jenkins-ci.org/redhat/jenkins-${JENKINS_VERSION}-1.1.noarch.rpm
 
 ENV JENKINS_HOME /var/lib/jenkins
 
 ADD jenkins.sh /srv/jenkins/jenkins.sh
+ADD jenkins_backup.sh /srv/jenkins/jenkins_backup.sh
 
 EXPOSE 8080
 VOLUME /var/lib/jenkins
--- jenkins.sh
+++ jenkins.sh
@@ -2,21 +2,28 @@
 
 : ${JENKINS_HOME_S3_BUCKET_NAME:-}
 
-jenkins_home_sync() {
+jenkins_home_restore() {
   if [[ -n ${JENKINS_HOME_S3_BUCKET_NAME} ]]; then
-    aws s3 sync \
-      --delete \
-      --exclude 'war/*' \
-      --exclude 'userContent/*' \
-      s3://${JENKINS_HOME_S3_BUCKET_NAME}/jenkins_home/ ${JENKINS_HOME}
+    # persist jenkins config backup bucket
+    echo "export JENKINS_HOME_S3_BUCKET_NAME=${JENKINS_HOME_S3_BUCKET_NAME}" > /etc/jenkins-bucket-config
+    [[ -n ${AWS_SECRET_ACCESS_KEY} ]] && echo "export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}" >> /etc/jenkins-bucket-config
+    [[ -n ${AWS_ACCESS_KEY_ID} ]] && echo "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}" >> /etc/jenkins-bucket-config
+
+    aws s3 cp s3://${JENKINS_HOME_S3_BUCKET_NAME}/jenkins_home/jenkins_home.tar.gz /tmp
+
+    if [[ -f /tmp/jenkins_home.tar.gz ]]; then
+      cd ${JENKINS_HOME}
+      tar -xzf /tmp/jenkins_home.tar.gz
+      rm -f /tmp/jenkins_home.tar.gz
+    fi
   else
-    echo 'Unable to sync existing jenkins configuration. JENKINS_HOME_S3_BUCKET_NAME is unset.'
+    echo 'Unable to restore existing jenkins configuration. JENKINS_HOME_S3_BUCKET_NAME is unset.'
   fi
 }
 
 # Allow to pass in jenkins options after --
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  jenkins_home_sync
+  jenkins_home_restore
 
   if [[ $? -eq 0 ]]; then
     exec java ${JAVA_OPTS} -jar /usr/lib/jenkins/jenkins.war ${JENKINS_OPTS} "$@"
